6987331a1ad7c4d9326adc658837c1ade529da93
Insomenya <kolowertnov.alexej@yandex.com>
2024-05-02 12:02:10 +0400

    added format to api requests

--- src/helpers/fetch-wrapper.js
+++ src/helpers/fetch-wrapper.js
@@ -17,7 +17,7 @@ function request(method) {
             requestOptions.headers['Content-Type'] = 'application/json';
             requestOptions.body = JSON.stringify(body);
         }
-        return fetch(url, requestOptions).then(handleResponse);
+        return fetch(url + '?format=json', requestOptions).then(handleResponse);
     }
 }
 
